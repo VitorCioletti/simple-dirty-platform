46009d83a9179e0defb811cedbfbf25b9b0699e9
VitorCioletti <ciolettiv@gmail.com>
2019-01-20 20:17:33 -0200

    Created new part of the map

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+	public int Health;
+
+	public int Speed;
+
+	protected Rigidbody2D Rigidbody2D;
+
+	protected Animator Animator;
+
+	protected bool IsMoving;
+
+	protected Transform Player;
+
+	void Awake()
+	{
+		Rigidbody2D = GetComponent<Rigidbody2D>();
+		Animator = GetComponent<Animator>();
+		Player = GameObject.Find("Player").GetComponent<Transform>();
+	}
+
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
